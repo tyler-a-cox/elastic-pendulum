bfbb617e40b3c461cda3c177b4ac36abddb5b598
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-07-06 14:26:56 -0700

    More formatting updates

--- pyelastic/animate.py
+++ pyelastic/animate.py
@@ -4,15 +4,17 @@ import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
-from _keys import FIG_DIR, VID_DIR
+from ._keys import FIG_DIR, VID_DIR
 from multiprocessing import cpu_count, Pool
-from double_pendulum import ElasticPendulum
+from .double_pendulum import ElasticPendulum
+
 
 class Animation:
-    """
-    """
-    def __init__(self, alpha=None, beta=None, seed=None, cores=None, fps=30,
-                 pendulums=1):
+    """ """
+
+    def __init__(
+        self, alpha=None, beta=None, seed=None, cores=None, fps=30, pendulums=1
+    ):
         self.dpi = 100
         self.size = 712
         self.fig_dir = FIG_DIR
@@ -38,72 +40,88 @@ class Animation:
             self.cores = cpu_count()
 
     def _plot_settings(self, x):
-            colors = np.zeros((x.shape[0], 4))
-            colors[:, 2] = 1
-            alpha = np.linspace(0.2, 0.8, x.shape[0]) ** 2.
-            colors[:, 3] = alpha
-            return colors
+        colors = np.zeros((x.shape[0], 4))
+        colors[:, 2] = 1
+        alpha = np.linspace(0.2, 0.8, x.shape[0]) ** 2.0
+        colors[:, 3] = alpha
+        return colors
 
     def plot_frame(self, d, i, trace=True, axes_off=True):
-        """
-        """
+        """ """
         colors = self._plot_settings(d.x1[:i])
 
         if trace:
-            plt.scatter(d.x2[:i], d.y2[:i], color=colors[:i], s=2.,
-                        zorder=0)
+            plt.scatter(d.x2[:i], d.y2[:i], color=colors[:i], s=2.0, zorder=0)
 
         if axes_off:
-            plt.axis('off')
+            plt.axis("off")
 
-        plt.plot([0, d.x1[i]], [0, d.y1[i]], color='black',
-                 zorder=1, linewidth=1., alpha=0.7)
-        plt.plot([d.x1[i], d.x2[i]], [d.y1[i], d.y2[i]],
-                 color='blue', zorder=1, linewidth=1., alpha=0.7)
-        plt.scatter([0, d.x1[i], d.x2[i]], [0, d.y1[i], d.y2[i]],
-                    color=((0,0,0,1),(0,0,0,1),(0, 0, 1, 1)), zorder=2)
-
+        plt.plot(
+            [0, d.x1[i]],
+            [0, d.y1[i]],
+            color="black",
+            zorder=1,
+            linewidth=1.0,
+            alpha=0.7,
+        )
+        plt.plot(
+            [d.x1[i], d.x2[i]],
+            [d.y1[i], d.y2[i]],
+            color="blue",
+            zorder=1,
+            linewidth=1.0,
+            alpha=0.7,
+        )
+        plt.scatter(
+            [0, d.x1[i], d.x2[i]],
+            [0, d.y1[i], d.y2[i]],
+            color=((0, 0, 0, 1), (0, 0, 0, 1), (0, 0, 1, 1)),
+            zorder=2,
+        )
 
     def clear_figs(self):
-        """
-        """
-        figs = glob.glob(os.path.join(self.fig_dir, '*png'))
+        """ """
+        figs = glob.glob(os.path.join(self.fig_dir, "*png"))
         for f in figs:
             os.remove(f)
 
     def main(self, i):
-        """
-        """
-        fig = plt.figure(figsize=(self.size / self.dpi, self.size / self.dpi),
-                         dpi=self.dpi)
+        """ """
+        fig = plt.figure(
+            figsize=(self.size / self.dpi, self.size / self.dpi), dpi=self.dpi
+        )
         for pendulum in self.pendulums:
             self.plot_frame(pendulum, i)
 
-        fig.set_size_inches(self.size / self.dpi, self.size/ self.dpi,
-                            forward=True)
+        fig.set_size_inches(self.size / self.dpi, self.size / self.dpi, forward=True)
 
         plt.xlim([self.mini_x * 1.05, self.maxi_x * 1.05])
         plt.ylim([self.mini_y * 1.05, self.maxi_y * 1.05])
 
         fig.tight_layout()
-        plt.savefig(os.path.join(self.fig_dir, str(i).zfill(5) + '.png'), dpi=self.dpi)
+        plt.savefig(os.path.join(self.fig_dir, str(i).zfill(5) + ".png"), dpi=self.dpi)
         plt.clf()
         plt.close()
 
     def save_movie(self):
-        """
-        """
+        """ """
         dt = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        self.fname = os.path.join(self.vid_dir, 'pend_{}.mp4'.format(dt))
-        figs = os.path.join(self.fig_dir, '%05d.png')
-        os.system('ffmpeg -r {} -f image2 -s 1920x1080 -i {} -vcodec \
-                    libx264 -crf 25  -pix_fmt yuv420p {}'.format(self.fps, figs, self.fname))
+        self.fname = os.path.join(self.vid_dir, "pend_{}.mp4".format(dt))
+        figs = os.path.join(self.fig_dir, "%05d.png")
+        os.system(
+            "ffmpeg -r {} -f image2 -s 1920x1080 -i {} -vcodec \
+                    libx264 -crf 25  -pix_fmt yuv420p {}".format(
+                self.fps, figs, self.fname
+            )
+        )
 
     def animate(self, save_movie=True):
         self.pendulums = []
 
         for b in self.beta:
-            ds = ElasticPendulum(dt=1. / self.fps, t_end=15., alpha_0=self.alpha, beta_0=b)
+            ds = ElasticPendulum(
+                dt=1.0 / self.fps, t_end=15.0, alpha_0=self.alpha, beta_0=b
+            )
             _ = ds.integrate()
             self.pendulums.append(ds)
 
