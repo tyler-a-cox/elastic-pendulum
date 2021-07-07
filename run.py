8017261ee5d744c9ec551986fbe67ff9344363e2
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-07-07 12:15:09 -0700

    Remove unnecessary functions

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
 from pyelastic import double_pendulum
 import time
 
+timings = {}
+
 start = time.time()
-pendulum = double_pendulum.ElasticPendulum(fps=24, t_end=20)
+pendulum = double_pendulum.ElasticPendulum(fps=24, t_end=10)
 _ = pendulum.integrate()
+endint = time.time()
+timings["integration"] = endint - start
 
 # for i in range(pendulum.x1.shape[0]):
 #    pendulum.save_frame(i, interpolate=False)
 pendulum.main_animate()
-print(time.time() - start)
+anttime = time.time()
+timings["animation"] = anttime - endint
+print("Integration time", timings["integration"])
+print("Animation time", timings["animation"])
+print("FPS:", 240 / timings["animation"])
 
 # pendulum.make_movie()
