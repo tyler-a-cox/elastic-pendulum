98dfd0b205649eee868ff1da89c8606d37e8fb84
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-07-07 15:11:52 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Elastic Double Pendulum
 
-Twitter bot that generates and posts videos of a double elastic pendulum.
+Twitter bot that generates and posts videos of a double elastic pendulum. The bot
+works by integrating the Lagrangian equations of motion for a two-dimensional double
+spring pendulum.
 
 <p align="center">
   <img src="assets/resized_example.gif" alt="animated" />
--- pyelastic/double_pendulum.py
+++ pyelastic/double_pendulum.py
@@ -169,7 +169,7 @@ class ElasticPendulum:
             self._b_pp(t, Y),
         ]
 
-    def integrate(self, method="RK45"):
+    def integrate(self, method="LSODA"):
         """Animate
 
         Args:
@@ -309,3 +309,95 @@ class ElasticPendulum:
         #    "basic_animation.mp4", fps=self.fps, extra_args=["-vcodec", "libx264"]
         # )
         anim.save("example_sim.gif", fps=self.fps, writer="Pillow")
+
+    def n_init(self):
+        """ """
+        self.line1.set_data([], [])
+        self.dot1.set_data([], [])
+        self.dot3.set_data([], [])
+
+        for j in range(self.ns):
+            self.trace_lc1[j].set_data([], [])
+            self.trace_lc2[j].set_data([], [])
+
+        return (self.line1,)
+
+    def n_animate(self, i):
+        """ """
+        self.line1.set_data([0, self.x1[i]], [0, self.y1[i]])
+        self.dot1.set_data(self.x1[i], self.y1[i])
+        self.dot3.set_data(0, 0)
+
+        for j in range(self.ns):
+            imin = i - (self.ns - j) * self.s
+            if imin < 0:
+                continue
+            imax = imin + self.s + 1
+            alpha = (j / self.ns) ** 2
+            self.trace_lc1[j].set_data(self.x1[imin:imax], self.y1[imin:imax])
+            self.trace_lc1[j].set_alpha(alpha)
+            self.trace_lc2[j].set_data(self.x2[imin:imax], self.y2[imin:imax])
+            self.trace_lc2[j].set_alpha(alpha)
+
+        return (self.line1,)
+
+    def main_n_animate(self, size=800, dpi=100):
+        """ """
+        self.fig = plt.figure(figsize=(size / dpi, size / dpi), dpi=dpi)
+        m1 = np.max([self.x1, self.x2])
+        m2 = np.max([self.y1, self.y2])
+        if m1 < 0:
+            m1 = 2
+        if m2 < 0:
+            m2 = 2
+
+        ax = plt.axes(
+            xlim=[np.min([self.x1, self.x2]), m1], ylim=[np.min([self.y1, self.y2]), m2]
+        )
+        ax.axis("off")
+        (self.line1,) = ax.plot([], [], lw=2, color="white", zorder=0)
+        (self.dot1,) = ax.plot([], [], color="white", marker="o", zorder=2)
+        (self.line2,) = ax.plot([], [], lw=2, color="magenta", zorder=0)
+        (self.dot2,) = ax.plot([], [], color="magenta", marker="o", zorder=2)
+        (self.dot3,) = ax.plot([], [], color="white", marker="o", zorder=2)
+        self.trace_lc1 = []
+        self.trace_lc2 = []
+        for _ in range(self.ns):
+            (trace1,) = ax.plot(
+                [],
+                [],
+                c="cyan",
+                solid_capstyle="round",
+                lw=1.5,
+                alpha=0,
+                zorder=0,
+            )
+            (trace2,) = ax.plot(
+                [],
+                [],
+                c="magenta",
+                solid_capstyle="round",
+                lw=1.5,
+                alpha=0,
+                zorder=0,
+            )
+            self.trace_lc1.append(trace1)
+            self.trace_lc2.append(trace2)
+
+        self.fig.set_size_inches(size / dpi, size / dpi, forward=True)
+        self.fig.tight_layout()
+
+        anim = animation.FuncAnimation(
+            self.fig,
+            self.single_animate,
+            init_func=self.single_init,
+            frames=self.x1.shape[0],
+            interval=0,
+            blit=True,
+            cache_frame_data=False,
+        )
+
+        # anim.save(
+        #    "basic_animation.mp4", fps=self.fps, extra_args=["-vcodec", "libx264"]
+        # )
+        anim.save("example_sim.gif", fps=self.fps, writer="pillow")
