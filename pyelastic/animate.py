52a917e40411b3c3e0d511a22483c58f60ab595b
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-07-07 16:12:46 -0700

    Reformat code

--- pyelastic/animate.py
+++ pyelastic/animate.py
@@ -7,8 +7,9 @@ from scipy.integrate import solve_ivp
 from multiprocessing import cpu_count, Pool
 from .double_pendulum import ElasticPendulum
 
-VID_DIR = "/Users/tyler/Projects/elastic-pendulum/assets/"
-FIG_DIR = "/Users/tyler/Projects/elastic-pendulum/assets/"
+VID_DIR = "assets"
+plt.rc("text", usetex=False)
+plt.style.use("dark_background")
 
 
 class Animation:
@@ -22,145 +23,36 @@ class Animation:
     """
 
     def __init__(
-        self, alpha=None, beta=None, seed=None, cores=None, fps=30, pendulums=1
+        self, alpha=None, beta=None, seed=None, cores=None, fps=24, np=1, tend=15
     ):
         self.dpi = 100
         self.size = 712
         self.fig_dir = FIG_DIR
         self.vid_dir = VID_DIR
         self.fps = fps
-        if seed is not None:
+        self.ns = 50
+        self.s = 4
+
+        if seed:
             np.random.seed(seed)
 
-        if alpha is not None:
+        if alpha:
             self.alpha = alpha
         else:
             self.alpha = np.random.uniform(-np.pi, np.pi)
 
-        if beta is not None:
+        if beta:
             self.beta = beta
         else:
             beta = np.random.uniform(-np.pi, np.pi)
             self.beta = np.linspace(beta, beta + 0.05, pendulums)
 
-        if cores is not None:
+        if cores:
             self.cores = cores
         else:
             self.cores = cpu_count()
 
-    def _plot_settings(self, x):
-        """Animate
-
-        Args:
-            save_movie : boolean, default=True
-
-        Returns:
-            None
-        """
-        colors = np.zeros((x.shape[0], 4))
-        colors[:, 2] = 1
-        alpha = np.linspace(0.2, 0.8, x.shape[0]) ** 2.0
-        colors[:, 3] = alpha
-        return colors
-
-    def plot_frame(self, d, i, trace=True, axes_off=True):
-        """Animate
-
-        Args:
-            save_movie : boolean, default=True
-
-        Returns:
-            None
-        """
-        colors = self._plot_settings(d.x1[:i])
-
-        if trace:
-            plt.scatter(d.x2[:i], d.y2[:i], color=colors[:i], s=2.0, zorder=0)
-
-        if axes_off:
-            plt.axis("off")
-
-        plt.plot(
-            [0, d.x1[i]],
-            [0, d.y1[i]],
-            color="black",
-            zorder=1,
-            linewidth=1.0,
-            alpha=0.7,
-        )
-        plt.plot(
-            [d.x1[i], d.x2[i]],
-            [d.y1[i], d.y2[i]],
-            color="blue",
-            zorder=1,
-            linewidth=1.0,
-            alpha=0.7,
-        )
-        plt.scatter(
-            [0, d.x1[i], d.x2[i]],
-            [0, d.y1[i], d.y2[i]],
-            color=((0, 0, 0, 1), (0, 0, 0, 1), (0, 0, 1, 1)),
-            zorder=2,
-        )
-
-    def clear_figs(self):
-        """Animate
-
-        Args:
-            save_movie : boolean, default=True
-
-        Returns:
-            None
-        """
-        figs = glob.glob(os.path.join(self.fig_dir, "*png"))
-        for f in figs:
-            os.remove(f)
-
-    def main(self, i):
-        """Animate
-
-        Args:
-            save_movie : boolean, default=True
-
-        Returns:
-            None
-        """
-        fig = plt.figure(
-            figsize=(self.size / self.dpi, self.size / self.dpi), dpi=self.dpi
-        )
-        for pendulum in self.pendulums:
-            self.plot_frame(pendulum, i)
-
-        fig.set_size_inches(self.size / self.dpi, self.size / self.dpi, forward=True)
-
-        plt.xlim([self.mini_x * 1.05, self.maxi_x * 1.05])
-        plt.ylim([self.mini_y * 1.05, self.maxi_y * 1.05])
-
-        fig.tight_layout()
-        plt.savefig(os.path.join(self.fig_dir, str(i).zfill(5) + ".png"), dpi=self.dpi)
-        plt.clf()
-        plt.close()
-
-    def save_movie(self):
-        """Animate
-
-        Args:
-            save_movie : boolean, default=True
-
-        Returns:
-            None
-        """
-        dt = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        self.fname = os.path.join(self.vid_dir, "pend_{}.mp4".format(dt))
-        figs = os.path.join(self.fig_dir, "%05d.png")
-        os.system(
-            "ffmpeg -r {} -f image2 -s 1920x1080 -i {} -vcodec \
-                    libx264 -crf 25  -pix_fmt yuv420p {}".format(
-                self.fps, figs, self.fname
-            )
-        )
-
-    def animate(self, save_movie=True):
+    def animate_npends(self, save_movie=True):
         """Animate
 
         Args:
@@ -191,9 +83,207 @@ class Animation:
             if np.max([d.y1, d.y2]) > self.maxi_y:
                 self.maxi_y = np.max([d.y1, d.y2])
 
-        self.clear_figs()
-        pool = Pool(processes=self.cores)
-        _ = pool.map(self.main, np.arange(self.pendulums[0].x1.shape[0]))
+    def single_init(self):
+        """ """
+        self.line1.set_data([], [])
+        self.dot1.set_data([], [])
+        self.line2.set_data([], [])
+        self.dot2.set_data([], [])
+        self.dot3.set_data([], [])
+        for j in range(self.ns):
+            self.trace_lc1[j].set_data([], [])
+            self.trace_lc2[j].set_data([], [])
+        return self.line1, self.dot1, self.line2, self.dot2, self.dot3
 
-        if save_movie:
-            self.save_movie()
+    def single_animate(self, i):
+        """ """
+        self.line1.set_data([0, self.pendulum.x1[i]], [0, self.pendulum.y1[i]])
+        self.dot1.set_data(self.pendulum.x1[i], self.pendulum.y1[i])
+        self.line2.set_data(
+            [self.pendulum.x1[i], self.pendulum.x2[i]],
+            [self.pendulum.y1[i], self.pendulum.y2[i]],
+        )
+        self.dot2.set_data(self.x2[i], self.y2[i])
+        self.dot3.set_data(0, 0)
+
+        for j in range(self.ns):
+            imin = i - (self.ns - j) * self.s
+            if imin < 0:
+                continue
+            imax = imin + self.s + 1
+            alpha = (j / self.ns) ** 2
+            self.trace_lc1[j].set_data(self.x1[imin:imax], self.y1[imin:imax])
+            self.trace_lc1[j].set_alpha(alpha)
+            self.trace_lc2[j].set_data(self.x2[imin:imax], self.y2[imin:imax])
+            self.trace_lc2[j].set_alpha(alpha)
+
+        return self.line1, self.dot1, self.line2, self.dot2, self.dot3
+
+    def main_animate(self, size=800, dpi=100, format="gif"):
+        """ """
+        assert format in ["gif", "mp4"], "Not a supported format"
+        self.pendulum = ElasticPendulum(fps=self.fps, t_end=self.tend)
+        _ = self.pendulum.integrate()
+
+        self.fig = plt.figure(figsize=(size / dpi, size / dpi), dpi=dpi)
+        m1 = np.max([self.pendulum.x1, self.pendulum.x2])
+        m2 = np.max([self.pendulum.y1, self.pendulum.y2])
+        if m1 < 0:
+            m1 = 2
+        if m2 < 0:
+            m2 = 2
+
+        ax = plt.axes(
+            xlim=[np.min([self.pendulum.x1, self.pendulum.x2]), m1],
+            ylim=[np.min([self.pendulum.y1, self.pendulum.y2]), m2],
+        )
+        ax.axis("off")
+        (self.line1,) = ax.plot([], [], lw=2, color="cyan", zorder=0)
+        (self.dot1,) = ax.plot([], [], color="cyan", marker="o", zorder=2)
+        (self.line2,) = ax.plot([], [], lw=2, color="magenta", zorder=0)
+        (self.dot2,) = ax.plot([], [], color="magenta", marker="o", zorder=2)
+        (self.dot3,) = ax.plot([], [], color="cyan", marker="o", zorder=2)
+        self.trace_lc1 = []
+        self.trace_lc2 = []
+        for _ in range(self.ns):
+            (trace1,) = ax.plot(
+                [],
+                [],
+                c="cyan",
+                solid_capstyle="round",
+                lw=1.5,
+                alpha=0,
+                zorder=0,
+            )
+            (trace2,) = ax.plot(
+                [],
+                [],
+                c="magenta",
+                solid_capstyle="round",
+                lw=1.5,
+                alpha=0,
+                zorder=0,
+            )
+            self.trace_lc1.append(trace1)
+            self.trace_lc2.append(trace2)
+
+        self.fig.set_size_inches(size / dpi, size / dpi, forward=True)
+        self.fig.tight_layout()
+
+        anim = animation.FuncAnimation(
+            self.fig,
+            self.single_animate,
+            init_func=self.single_init,
+            frames=pendulum.x1.shape[0],
+            interval=0,
+            blit=True,
+            cache_frame_data=False,
+        )
+
+        if format == "gif":
+            anim.save(os.path.join(VID_DIR, "sim.gif"), fps=self.fps)
+
+        else:
+            anim.save(
+                os.path.join(VID_DIR, "sim.mp4"),
+                fps=self.fps,
+                extra_args=["-vcodec", "libx264"],
+            )
+
+    def n_init(self):
+        """ """
+        self.line1.set_data([], [])
+        self.dot1.set_data([], [])
+        self.dot3.set_data([], [])
+
+        for j in range(self.ns):
+            self.trace_lc1[j].set_data([], [])
+            self.trace_lc2[j].set_data([], [])
+
+        return (self.line1,)
+
+    def n_animate(self, i):
+        """ """
+        self.line1.set_data([0, self.x1[i]], [0, self.y1[i]])
+        self.dot1.set_data(self.x1[i], self.y1[i])
+        self.dot3.set_data(0, 0)
+
+        for j in range(self.ns):
+            imin = i - (self.ns - j) * self.s
+            if imin < 0:
+                continue
+            imax = imin + self.s + 1
+            alpha = (j / self.ns) ** 2
+            self.trace_lc1[j].set_data(self.x1[imin:imax], self.y1[imin:imax])
+            self.trace_lc1[j].set_alpha(alpha)
+            self.trace_lc2[j].set_data(self.x2[imin:imax], self.y2[imin:imax])
+            self.trace_lc2[j].set_alpha(alpha)
+
+        return (self.line1,)
+
+    def main_n_animate(self, size=800, dpi=100, format="gif"):
+        """ """
+        assert format in ["gif", "mp4"], "Not a supported format"
+        self.fig = plt.figure(figsize=(size / dpi, size / dpi), dpi=dpi)
+        m1 = np.max([self.x1, self.x2])
+        m2 = np.max([self.y1, self.y2])
+        if m1 < 0:
+            m1 = 2
+        if m2 < 0:
+            m2 = 2
+
+        ax = plt.axes(
+            xlim=[np.min([self.x1, self.x2]), m1], ylim=[np.min([self.y1, self.y2]), m2]
+        )
+        ax.axis("off")
+        (self.line1,) = ax.plot([], [], lw=2, color="white", zorder=0)
+        (self.dot1,) = ax.plot([], [], color="white", marker="o", zorder=2)
+        (self.line2,) = ax.plot([], [], lw=2, color="magenta", zorder=0)
+        (self.dot2,) = ax.plot([], [], color="magenta", marker="o", zorder=2)
+        (self.dot3,) = ax.plot([], [], color="white", marker="o", zorder=2)
+        self.trace_lc1 = []
+        self.trace_lc2 = []
+        for _ in range(self.ns):
+            (trace1,) = ax.plot(
+                [],
+                [],
+                c="cyan",
+                solid_capstyle="round",
+                lw=1.5,
+                alpha=0,
+                zorder=0,
+            )
+            (trace2,) = ax.plot(
+                [],
+                [],
+                c="magenta",
+                solid_capstyle="round",
+                lw=1.5,
+                alpha=0,
+                zorder=0,
+            )
+            self.trace_lc1.append(trace1)
+            self.trace_lc2.append(trace2)
+
+        self.fig.set_size_inches(size / dpi, size / dpi, forward=True)
+        self.fig.tight_layout()
+
+        anim = animation.FuncAnimation(
+            self.fig,
+            self.single_animate,
+            init_func=self.single_init,
+            frames=self.x1.shape[0],
+            interval=0,
+            blit=True,
+            cache_frame_data=False,
+        )
+
+        if format == "gif":
+            anim.save(os.path.join(VID_DIR, "sim.gif"), fps=self.fps)
+
+        else:
+            anim.save(
+                os.path.join(VID_DIR, "sim.mp4"),
+                fps=self.fps,
+                extra_args=["-vcodec", "libx264"],
+            )
--- pyelastic/double_pendulum.py
+++ pyelastic/double_pendulum.py
@@ -8,8 +8,7 @@ from scipy.integrate import solve_ivp
 from scipy.interpolate import interp1d
 from multiprocessing import cpu_count, Pool
 
-VID_DIR = "/Users/tyler/Projects/elastic-pendulum/assets/"
-FIG_DIR = "/Users/tyler/Projects/elastic-pendulum/assets/"
+VID_DIR = "assets"
 
 plt.rc("text", usetex=False)
 plt.style.use("dark_background")
@@ -58,8 +57,6 @@ class ElasticPendulum:
         self.b0 = 1.0
         self.a1 = 0.0
         self.b1 = 0.0
-        self.ns = 50
-        self.s = 4
         self.t_end = t_end
 
         #
@@ -192,7 +189,7 @@ class ElasticPendulum:
         self.solution = solve_ivp(
             self._inte, [0, self.t_end], Y0, t_eval=self.t_eval, method=method
         )
-        return self.cartesian(self.solution.y[[0, 2, 4, 6]].T)
+        self.cartesian(self.solution.y[[0, 2, 4, 6]].T)
 
     def cartesian(self, array):
         """Animate
@@ -212,193 +209,3 @@ class ElasticPendulum:
         self.fy1 = interp1d(np.arange(0, self.x1.shape[0]), self.y1)
         self.fx2 = interp1d(np.arange(0, self.x1.shape[0]), self.x2)
         self.fy2 = interp1d(np.arange(0, self.x1.shape[0]), self.y2)
-        return self.x1, self.y1, self.x2, self.y2
-
-    def single_init(self):
-        """ """
-        self.line1.set_data([], [])
-        self.dot1.set_data([], [])
-        self.line2.set_data([], [])
-        self.dot2.set_data([], [])
-        self.dot3.set_data([], [])
-        for j in range(self.ns):
-            self.trace_lc1[j].set_data([], [])
-            self.trace_lc2[j].set_data([], [])
-        return self.line1, self.dot1, self.line2, self.dot2, self.dot3
-
-    def single_animate(self, i):
-        """ """
-        self.line1.set_data([0, self.x1[i]], [0, self.y1[i]])
-        self.dot1.set_data(self.x1[i], self.y1[i])
-        self.line2.set_data(
-            [self.x1[i], self.x2[i]],
-            [self.y1[i], self.y2[i]],
-        )
-        self.dot2.set_data(self.x2[i], self.y2[i])
-        self.dot3.set_data(0, 0)
-
-        for j in range(self.ns):
-            imin = i - (self.ns - j) * self.s
-            if imin < 0:
-                continue
-            imax = imin + self.s + 1
-            alpha = (j / self.ns) ** 2
-            self.trace_lc1[j].set_data(self.x1[imin:imax], self.y1[imin:imax])
-            self.trace_lc1[j].set_alpha(alpha)
-            self.trace_lc2[j].set_data(self.x2[imin:imax], self.y2[imin:imax])
-            self.trace_lc2[j].set_alpha(alpha)
-
-        return self.line1, self.dot1, self.line2, self.dot2, self.dot3
-
-    def main_animate(self, size=800, dpi=100):
-        """ """
-        self.fig = plt.figure(figsize=(size / dpi, size / dpi), dpi=dpi)
-        m1 = np.max([self.x1, self.x2])
-        m2 = np.max([self.y1, self.y2])
-        if m1 < 0:
-            m1 = 2
-        if m2 < 0:
-            m2 = 2
-
-        ax = plt.axes(
-            xlim=[np.min([self.x1, self.x2]), m1], ylim=[np.min([self.y1, self.y2]), m2]
-        )
-        ax.axis("off")
-        (self.line1,) = ax.plot([], [], lw=2, color="cyan", zorder=0)
-        (self.dot1,) = ax.plot([], [], color="cyan", marker="o", zorder=2)
-        (self.line2,) = ax.plot([], [], lw=2, color="magenta", zorder=0)
-        (self.dot2,) = ax.plot([], [], color="magenta", marker="o", zorder=2)
-        (self.dot3,) = ax.plot([], [], color="cyan", marker="o", zorder=2)
-        self.trace_lc1 = []
-        self.trace_lc2 = []
-        for _ in range(self.ns):
-            (trace1,) = ax.plot(
-                [],
-                [],
-                c="cyan",
-                solid_capstyle="round",
-                lw=1.5,
-                alpha=0,
-                zorder=0,
-            )
-            (trace2,) = ax.plot(
-                [],
-                [],
-                c="magenta",
-                solid_capstyle="round",
-                lw=1.5,
-                alpha=0,
-                zorder=0,
-            )
-            self.trace_lc1.append(trace1)
-            self.trace_lc2.append(trace2)
-
-        self.fig.set_size_inches(size / dpi, size / dpi, forward=True)
-        self.fig.tight_layout()
-
-        anim = animation.FuncAnimation(
-            self.fig,
-            self.single_animate,
-            init_func=self.single_init,
-            frames=self.x1.shape[0],
-            interval=0,
-            blit=True,
-            cache_frame_data=False,
-        )
-
-        # anim.save(
-        #    "basic_animation.mp4", fps=self.fps, extra_args=["-vcodec", "libx264"]
-        # )
-        anim.save("example_sim.gif", fps=self.fps, writer="Pillow")
-
-    def n_init(self):
-        """ """
-        self.line1.set_data([], [])
-        self.dot1.set_data([], [])
-        self.dot3.set_data([], [])
-
-        for j in range(self.ns):
-            self.trace_lc1[j].set_data([], [])
-            self.trace_lc2[j].set_data([], [])
-
-        return (self.line1,)
-
-    def n_animate(self, i):
-        """ """
-        self.line1.set_data([0, self.x1[i]], [0, self.y1[i]])
-        self.dot1.set_data(self.x1[i], self.y1[i])
-        self.dot3.set_data(0, 0)
-
-        for j in range(self.ns):
-            imin = i - (self.ns - j) * self.s
-            if imin < 0:
-                continue
-            imax = imin + self.s + 1
-            alpha = (j / self.ns) ** 2
-            self.trace_lc1[j].set_data(self.x1[imin:imax], self.y1[imin:imax])
-            self.trace_lc1[j].set_alpha(alpha)
-            self.trace_lc2[j].set_data(self.x2[imin:imax], self.y2[imin:imax])
-            self.trace_lc2[j].set_alpha(alpha)
-
-        return (self.line1,)
-
-    def main_n_animate(self, size=800, dpi=100):
-        """ """
-        self.fig = plt.figure(figsize=(size / dpi, size / dpi), dpi=dpi)
-        m1 = np.max([self.x1, self.x2])
-        m2 = np.max([self.y1, self.y2])
-        if m1 < 0:
-            m1 = 2
-        if m2 < 0:
-            m2 = 2
-
-        ax = plt.axes(
-            xlim=[np.min([self.x1, self.x2]), m1], ylim=[np.min([self.y1, self.y2]), m2]
-        )
-        ax.axis("off")
-        (self.line1,) = ax.plot([], [], lw=2, color="white", zorder=0)
-        (self.dot1,) = ax.plot([], [], color="white", marker="o", zorder=2)
-        (self.line2,) = ax.plot([], [], lw=2, color="magenta", zorder=0)
-        (self.dot2,) = ax.plot([], [], color="magenta", marker="o", zorder=2)
-        (self.dot3,) = ax.plot([], [], color="white", marker="o", zorder=2)
-        self.trace_lc1 = []
-        self.trace_lc2 = []
-        for _ in range(self.ns):
-            (trace1,) = ax.plot(
-                [],
-                [],
-                c="cyan",
-                solid_capstyle="round",
-                lw=1.5,
-                alpha=0,
-                zorder=0,
-            )
-            (trace2,) = ax.plot(
-                [],
-                [],
-                c="magenta",
-                solid_capstyle="round",
-                lw=1.5,
-                alpha=0,
-                zorder=0,
-            )
-            self.trace_lc1.append(trace1)
-            self.trace_lc2.append(trace2)
-
-        self.fig.set_size_inches(size / dpi, size / dpi, forward=True)
-        self.fig.tight_layout()
-
-        anim = animation.FuncAnimation(
-            self.fig,
-            self.single_animate,
-            init_func=self.single_init,
-            frames=self.x1.shape[0],
-            interval=0,
-            blit=True,
-            cache_frame_data=False,
-        )
-
-        # anim.save(
-        #    "basic_animation.mp4", fps=self.fps, extra_args=["-vcodec", "libx264"]
-        # )
-        anim.save("example_sim.gif", fps=self.fps, writer="pillow")
