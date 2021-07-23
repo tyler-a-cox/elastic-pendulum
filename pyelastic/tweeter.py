6be5a6d6a153aebb0e3b1d246ffecbd1ca57ae4e
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-07-23 16:21:57 -0700

    Path changes

--- pyelastic/tweeter.py
+++ pyelastic/tweeter.py
@@ -45,7 +45,7 @@ def update_status(filename=None):
         print(pendulum.fname)
 
 
-def media(filename=None, number=1):
+def media(filename=None, number=1, clean=False):
     """Animate
 
     Args:
@@ -57,32 +57,37 @@ def media(filename=None, number=1):
     if filename is None and number <= 1:
         pendulum = Animation(fps=60, tend=10.0)
         pendulum.main_animate()
-        status = "Starting angles {} degrees and {} degrees".format(
-            np.round(np.rad2deg(pendulum.alpha), 2),
-            np.round(np.rad2deg(pendulum.beta), 2),
+        status = (
+            "Starting angles {} degrees and {} degrees\n\n#science #physics".format(
+                np.round(np.rad2deg(pendulum.alpha), 2),
+                np.round(np.rad2deg(pendulum.beta), 2),
+            )
         )
         filename = pendulum.filename
 
     elif filename is None and number > 1:
         cmap = np.random.choice(cmaps)
         offset = np.random.uniform(1e-5, 1e-4)
-        pendulum = Animation(fps=60, tend=10.0, npends=5, offset=offset)
-        status = "5 double pendulums separated by {} degrees".format(
-            np.round(np.rad2deg(offset), 4)
+        pendulum = Animation(fps=60, tend=10.0, npends=number, offset=offset)
+        status = (
+            "6 double pendulums separated by {} degrees\n\n#science #physics".format(
+                np.round(np.rad2deg(offset), 4)
+            )
         )
         pendulum.main_n_animate(cmap=cmap)
         filename = pendulum.filename
 
     video = open(filename, "rb")
     response = twitter.upload_video(media=video, media_type="video/gif")
+
+    if clean:
+        os.remove(filename)
+
     return status, response
 
 
 def post_content(clean=False):
     """ """
-    npend = np.random.choice([1, 5], p=[0.25, 0.75])
-    status, response = media(number=npend)
+    npend = np.random.choice([1, 6], p=[0.25, 0.75])
+    status, response = media(number=npend, clean=clean)
     twitter.update_status(status=status, media_ids=[response["media_id"]])
-
-    if clean:
-        os.remove()
