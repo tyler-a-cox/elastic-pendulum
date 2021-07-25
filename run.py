f6a7817268d9601dd46a7ca9143055e20d8c744c
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-07-24 23:50:16 -0700

    Update bot

--- run.py
+++ run.py
@@ -1,10 +1,4 @@
-import time
-import numpy as np
-import cmasher as cm
-import pylab as plt
 from pyelastic.tweeter import post_content
-from pyelastic.animate import Animation
-
 
 if __name__ == "__main__":
     post_content(clean=True)
